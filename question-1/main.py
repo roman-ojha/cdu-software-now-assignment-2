27ed6ffda723e081a4211f03dc4b4a0825d30de3
awais-357 <awais357ahmed@gmail.com>
2026-01-14 23:36:45 +1100

    added decryption algorithm

--- question-1/main.py
+++ question-1/main.py
@@ -1,5 +1,39 @@
 def encrypt_text():
-    pass
+    """
+    We will encrypt the text preset in raw_text.txt file by using double shift rules.
+    Store the encrypted text into encrypted_text.txt file.
+    Also store the shifts used for each character in shifts.txt file for decryption purpose.
+    """
+
+    # reading shifts from user input
+    shift1 = int(input("Enter shift 1: "))
+    shift2 = int(input("Enter shift 2: "))
+
+    # reading raw text
+    with open("raw_text.txt", "r", encoding="utf-8") as f:
+        raw = f.read()
+
+    encrypted = ""
+    shifts = []
+    for c in raw:
+        if c.islower():
+            s = shift1*shift2 if 'a' <= c <= 'm' else -(shift1+shift2)
+        elif c.isupper():
+            s = -shift1 if 'A' <= c <= 'M' else shift2**2
+        else:
+            s = 0
+        encrypted += chr((ord(c) + s) % 256)
+        shifts.append(s)
+
+    # storing encrypted text
+    with open("encrypted_text.txt", "w", encoding="utf-8") as f:
+        f.write(encrypted)
+
+    # storing shifts into shifts.txt
+    with open("shifts.txt", "w", encoding="utf-8") as f:
+        f.write(" ".join(map(str, shifts)))
+
+    print("Successfully encrypted")
 
 
 def decrypt_text():
